b025be1b1a2b42352bfe68f84e7a676bfc966b0f
Matt Przybylski <matt@reintroducing.com>
2015-01-23 13:22:11 -0600

    fixed watch task and removed jquery since it gets pulled in through npm

--- root/gulp/tasks/watch.js
+++ root/gulp/tasks/watch.js
@@ -4,7 +4,11 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync'),
     config = require('../config.json');
 
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
 gulp.task('watch', ['browserify'], function() {
-    gulp.watch(config.dev + 'index.html', [browserSync.reload()]);
+    gulp.watch(config.dev + 'index.html', ['bs-reload']);
     gulp.watch(config.sass + '/**/*.scss', ['compass-dev']);
 });
